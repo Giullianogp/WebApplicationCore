cc7a14646b3122008d9776af446729ad3e445aab
Giulliano G. Pivetta <giullianogp@hotmail.com>
2018-03-26 17:42:15 -0300

    Add project files.

---
+++ WebApplicationCore/Models/MockPieRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplicationCore.Models
+{
+    public class MockPieRepository : IPieRepository
+    {
+        private IEnumerable<Pie> _pies;
+
+        public MockPieRepository()
+        {
+            if (_pies == null)
+            {
+                InitializePies();
+            }
+        }
+
+        private void InitializePies()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Pie> GetAllPies()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Pie GetPieById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
